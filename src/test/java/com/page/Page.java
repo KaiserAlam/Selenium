1920a3c4567e761bdec0b862222f1ed24f9ed76b
Kaiser Alam <kaiseralam@msn.com>
2017-10-26 00:12:30 -0700

    remove spaces

--- src/test/java/com/page/Page.java
+++ src/test/java/com/page/Page.java
@@ -3,8 +3,6 @@ package com.page;
 import java.util.ResourceBundle;
 
 public abstract class Page {
-
     public abstract ResourceBundle getResourceBundle();
     public abstract String getURL();
-
 }
